9cb1fcf2be6981821cc4774e69e1caa6032fef2c
ultimate-scope-5575 <a.bystranowska.takahashi@gmail.com>
2020-04-16 17:02:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ function theBeatlesPlay(musicians, instruments){
 }
 function johnLennonFacts(facts) {
   var arr = [];
+  facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
   var i = 0;
   while ( i < facts.length){
     var fact = facts[i] + '!!!';
