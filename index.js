a85c3c5e248dec8d8cec7483e2ad771fad3720d8
ultimate-scope-5575 <a.bystranowska.takahashi@gmail.com>
2020-04-16 16:38:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var arr = [];
+  for (var i = 0; i < musicians.length; i++){
+    var musician = musicians[i];
+    var instrument = instruments[i]
+    var solution = musician + ' plays '+ instrument
+    arr.push(solution)
+  }
+  return arr;
+}
