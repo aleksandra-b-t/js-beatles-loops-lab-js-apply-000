258727619a367d918c770454d42efd5ae53c39fa
ultimate-scope-5575 <a.bystranowska.takahashi@gmail.com>
2020-04-16 17:05:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,12 +11,6 @@ function theBeatlesPlay(musicians, instruments){
 }
 function johnLennonFacts(facts) {
   var arr = [];
-//   facts = [
-//   "He was the last Beatle to learn to drive",
-//   "He was never a vegetarian",
-//   "He was a choir boy and boy scout",
-//   "He hated the sound of his own voice"
-// ];
   var i = 0;
   while (i < facts.length){
     var fact = facts[i] + '!!!';
@@ -25,16 +19,16 @@ function johnLennonFacts(facts) {
   }
   return arr;
 }
-// function iLoveTheBeatles(n) {
-//   var arr = [];
-//   if (n < 15){
-//   var i = 15 - n;
-//   do {
-//     while (i > 0){
-//     arr.push('I love the Beatles!');
-//       }
-//   i--;
-//   }
-//   }
-//   return arr;
-// }
+function iLoveTheBeatles(n) {
+ var arr = [];
+ if (n < 15){
+ var i = 15 - n;
+  do {
+  while (i > 0){
+    arr.push('I love the Beatles!');
+   i--;
+   }
+  }
+  }
+ return arr;
+}
